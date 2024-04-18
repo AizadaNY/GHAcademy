adfc0108319d0a7d5076d108ba89774882c9b20f
AizadaNY <aizada.abdyrakhmanova@gmail.com>
2024-04-17 21:45:18 -0400

    maxelement

---
+++ src/main/java/nasm/arrays/MaxElementInArray.java
@@ -0,0 +1,24 @@
+package nasm.arrays;
+
+public class MaxElementInArray {
+/*
+//code using C
+public void maxElement(int arr,int length){
+    int *ptr=arr;
+    int max=*ptr;
+
+    start_loop:
+      if(ptr<arr+length){
+         if (*ptr>max){
+         max=*ptr;
+        }
+         ptr++;
+         goto start_loop
+      }
+
+}
+ */
+
+
+
+}
