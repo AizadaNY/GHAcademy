3dcd43da6cd1149b5d27e304626279578a4cd2f3
AizadaNY <aizada.abdyrakhmanova@gmail.com>
2024-04-30 22:41:14 -0400

    matrix homework

---
+++ src/main/java/coding/homework/Matrix.java
@@ -0,0 +1,24 @@
+package coding.homework;
+
+public class Matrix {
+
+   /*
+   Implement Basic Matrix multiplication
+    */
+
+    public int[][] multiplication(int[][] matrixA, int[][] matrixB) {
+
+        int rowA=matrixA.length;
+        int rowB=matrixB.length;
+        int[][] result=new int[rowA][rowB];
+
+        for (int i = 0; i < rowA; i++) {
+            for (int j = 0; j < rowB; j++) {
+                matrixA[rowA]
+            }
+        }
+
+
+
+    }
+}
