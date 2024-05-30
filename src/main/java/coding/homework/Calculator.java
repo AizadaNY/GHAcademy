22ad803198efb9da7261ac0981ce74c8d0ef2d49
AizadaNY <aizada.abdyrakhmanova@gmail.com>
2024-05-29 22:45:29 -0400

    CS homework"

--- src/main/java/coding/homework/Calculator.java
+++ src/main/java/coding/homework/Calculator.java
@@ -1,6 +1,7 @@
 package coding.homework;
 
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Calculator {
@@ -8,14 +9,19 @@ public class Calculator {
     public static void main(String[] args) {
 
         Scanner sc=new Scanner(System.in);
-        String input=sc.nextLine();
+        String input=sc.nextLine().toLowerCase();
         String [] arrInput=input.split(" ");
         double result=0;
         double num1=Double.parseDouble(arrInput[1]);
         double num2=Double.parseDouble(arrInput[2]);
-        switch (arrInput[0]){
+        String sign=arrInput[0].trim().toUpperCase();
+        System.out.println(sign);
+        System.out.println(num1);
+        System.out.println(num2);
+        switch (sign){
             case "ADD":
                 result=num1+num2;
+
             case "MUL":
                 result=num1*num2;
             case "DIV":
@@ -23,24 +29,23 @@ public class Calculator {
             case "SUB":
                 result=num1-num2;
             case "MOD":
-                result=num1/num2;
+                double div=num1/num2;
+                Integer i=Double.valueOf(div);
+                result=num1-(div*num2);
 
             case "POW":
+                double powResult=1;
                 for (int i = 1; i < num2; i++) {
-
+                    powResult=powResult*num1;
                 }
+                result=powResult;
         }
-        System.out.println("Result is: "+result);
+        System.out.println("Result is "+result);
+
 
 
 
     }
 
-    public double power(int a,int b){
 
-        while (b>1){
-           int result=a;
-        }
-
-    }
 }
