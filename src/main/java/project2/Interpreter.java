753292c6855deddb71d8f4fc6ea201b2236b923e
AizadaNY <aizada.abdyrakhmanova@gmail.com>
2024-06-30 13:11:59 -0400

    p2

---
+++ src/main/java/project2/Interpreter.java
@@ -0,0 +1,56 @@
+package project2;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+public class Interpreter {
+
+    private final Map<String,Integer> valueTable=new HashMap<>();
+
+    int visit(ASTNode node){
+        if(node instanceof BinaryOpNode){
+            int left=visit(((BinaryOpNode) node).leftNode);
+            int right=visit(((BinaryOpNode) node).rightNode);
+            switch (((BinaryOpNode) node).operationToken.type){
+                case ADDITION: {
+                    return left-right;
+                }
+                case PLUS: {
+                    return left+right;
+                }
+                case DIVISION: {
+                    return left/right;
+                }
+                case MULTIPLY: {
+                    return left*right;
+                }
+                default: throw new ParserException("Unexpected token ");
+            }
+        }else if(node instanceof NumberNode){
+            return ((NumberNode) node).value;
+        }else if(node instanceof VarDecl ){
+             int rightExpressionResult=visit(((VarDecl) node).expression);
+             valueTable.put(((VarDecl) node).varNode.name , rightExpressionResult);
+             return rightExpressionResult;
+        }else if(node instanceof Var){
+              String varName= ((Var) node).name;
+              if(!valueTable.containsKey(varName)){
+                  throw  new ParserException("Variable not found " + varName);
+              }
+              return valueTable.get(varName);
+        }else if(node instanceof Assign){
+              int rightExpression=visit(((Assign) node).right);
+              valueTable.put(((Assign) node).left.name, rightExpression);
+              return rightExpression;
+        }else if(node instanceof Block){
+             int result=0;
+             for(ASTNode statement: ((Block) node).statements){
+                 result=visit(statement);
+             }
+             return result;
+        }else{
+
+        }
+    }
+}
--- src/main/java/project2/SemanticAnalyzer.java
+++ src/main/java/project2/SemanticAnalyzer.java
@@ -1,6 +1,7 @@
 package project2;
 
 import javax.lang.model.element.VariableElement;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.Stack;
 
@@ -14,16 +15,41 @@ public class SemanticAnalyzer {
             visit(((BinaryOpNode) node).rightNode);
         }else if(node instanceof NumberNode){
             //Nothing
-        }else if(node instanceof VarDecl){
-
+        }else if(node instanceof VarDecl ){
+            String varName=((VarDecl) node).varNode.name;
+            if(isVariableDefined(varName)){
+                throw new ParserException("Identifier already defined: "+ varName);
+            }
+            scopes.peek().add(varName);// declare variable in scope
         }else if(node instanceof Var){
-
+            String varName=((Var) node).name;
+            if(!isVariableDefined(varName)){
+                throw new ParserException("Unexpected identifier: "+ varName);
+            }
         }else if(node instanceof Assign){
-
+            Assign assignNode=(Assign) node;
+            String varName=assignNode.left.name;
+            if(!isVariableDefined(varName)){
+                throw new ParserException("Unexpected identifier: "+ varName);
+            }
+            visit(assignNode.right);
         }else if(node instanceof Block){
-
+            scopes.push(new HashSet<>()); //enter a new scope
+            for (ASTNode statement: ((Block) node).statements) {
+                visit(statement);
+            }
+            scopes.pop(); //exit scope
         }else{
             throw new ParserException("Unexpected AST Node "+ node.getClass().getCanonicalName());
         }
+    }
+
+    private boolean isVariableDefined(String varName) {
+        for (Set<String> scope:scopes) {
+            if(scope.contains(varName)){
+                return true;
+            }
+        }
+        return false;
     }
 }
