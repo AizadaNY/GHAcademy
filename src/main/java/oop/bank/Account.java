bdc6d3a645bd07df209b747de1dd73b5da6763dd
AizadaNY <aizada.abdyrakhmanova@gmail.com>
2024-07-17 22:43:00 -0400

    OOP

---
+++ src/main/java/oop/bank/Account.java
@@ -0,0 +1,21 @@
+package oop.bank;
+
+public class Account implements Bank {
+     String name;
+     String number;
+
+    @Override
+    public void balance() {
+
+    }
+
+    @Override
+    public void deposit() {
+
+    }
+
+    @Override
+    public void withdraw() {
+
+    }
+}
